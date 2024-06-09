1753f687ac7f50b5abbea39c0ca5ffec95bab507
mihailsarenko <mixail.sharenko@gmail.com>
2024-06-09 22:08:22 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Modal from './Modal'
-import Sidebar from './Sidebar'
-import Home from './Home'
-
+import React from 'react';
+import Modal from './Modal';
+import Sidebar from './Sidebar';
+import Home from './Home';
 function App() {
     return (
-        <main>
-            <Home/>
-            {/*<Modal/>
-            <Sidebar/>*/}
-        </main>
-    )
+        <>
+            <Home />
+            <Modal />
+            <Sidebar />
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,19 @@
-import React, { useContext } from 'react'
-import {useGlobalContext} from './context';
-import { FaBars } from 'react-icons/fa'
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 
 const Home = () => {
-  const {openSideBar, openModal} = useGlobalContext();
-  return <main>
-    <button onClick={openSideBar} className='sidebar-toggle'>
-      <FaBars />
-    </button
-    ><button onClick={openModal} className='btn'>
-      Show modal
-    </button>
-  </main>
-}
+  const { openSidebar, openModal } = useGlobalContext();
+  return (
+      <main>
+        <button onClick={openSidebar} className='sidebar-toggle'>
+          <FaBars />
+        </button>
+        <button onClick={openModal} className='btn'>
+          show modal
+        </button>
+      </main>
+  );
+};
 
-export default Home
+export default Home;
